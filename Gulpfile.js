76c28cf8cfff29fd411cd4bc34a67a296bb4fdb2
Gary Jones <gary@gamajo.com>
2017-01-17 08:18:09 +0000

    Switch gruntfile for gulpfile

---
+++ Gulpfile.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var gulp    = require( 'gulp' ),
+    pkg     = require( './package.json' ),
+    toolkit = require( 'gulp-wp-toolkit' ),
+	zip     = require( 'gulp-zip' );
+
+require( 'gulp-stats' )( gulp );
+
+toolkit.extendConfig( {
+	theme: {
+		name: "Utility Pro",
+		homepage: pkg.homepage,
+		description: pkg.description,
+		author: pkg.author,
+		version: pkg.version,
+		license: pkg.license,
+		textdomain: pkg.name
+    },
+	src: {
+		zipuser: [
+			'images/*',
+			'includes/**/*',
+			'includes-vendors/**/*',
+			'js/*',
+			'languages/*',
+			'front-page.php',
+			'functions.php',
+			'LICENSE.md',
+			'page_landing.php',
+			'readme.txt',
+			'screenshot.png',
+			'style*'
+		],
+		zipdev: [
+			'develop/images/*',
+			'develop/js/*',
+			'develop/scss/**/*',
+			'images/*',
+			'includes/**/*',
+			'includes-vendors/**/*',
+			'js/*',
+			'languages/**',
+			'.bowerrc',
+			'bower.json',
+			'CHANGELOG.md',
+			'composer.json',
+			'composer.lock',
+			'front-page.php',
+			'functions.php',
+			'Gulpfile.js',
+			'LICENSE.md',
+			'package.json',
+			'page_landing.php',
+			'readme.txt',
+			'README.md',
+			'screenshot.png',
+			'style*'
+		]
+	},
+} );
+
+toolkit.extendTasks( gulp, { /* gulp task overrides */
+	'pre-commit': '',
+	'zip': [[ 'zipuser', 'zipdev' ]],
+	'zipuser': function() {
+		return gulp.src( toolkit.config.src.zipuser, { base: './' } )
+			.pipe( zip( pkg.name + '-' + pkg.version + '.zip' ) )
+			.pipe( gulp.dest( 'dist' ) );
+	},
+	'zipdev': function() {
+		return gulp.src( toolkit.config.src.zipdev, { base: './' } )
+			.pipe( zip( pkg.name + '-developer-' + pkg.version + '.zip' ) )
+			.pipe( gulp.dest( 'dist' ) );
+	}
+} );
