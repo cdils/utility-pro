5b84529d5291b25c0f16124697b97e7684c25e18
Gary Jones <gary@gamajo.com>
2017-04-08 00:50:37 +0100

    Improve SearchForm class

--- src/SearchForm.php
+++ src/SearchForm.php
@@ -2,20 +2,24 @@
 /**
  * Utility Pro.
  *
- * @package      Utility_Pro
+ * @package      CDils\UtilityPro
  * @link         http://www.carriedils.com/utility-pro
  * @author       Carrie Dils
  * @copyright    Copyright (c) 2015, Carrie Dils
  * @license      GPL-2.0+
  */
 
+declare( strict_types = 1 );
+
+namespace CDils\UtilityPro;
+
 /**
  * Search form class.
  *
- * @package Utility_Pro
+ * @package CDils\UtilityPro
  * @author Gary Jones
  */
-class Utility_Pro_Search_Form {
+class SearchForm {
 	/**
 	 * Unique ID for this search field.
 	 *
@@ -37,15 +41,15 @@ class Utility_Pro_Search_Form {
 	 *
 	 * @param array $strings Optional. Array of strings. Default is an empty array.
 	 */
-	public function __construct( array $strings = array() ) {
-		$default_strings = array(
+	public function __construct( array $strings = [] ) {
+		$default_strings = [
 			/** This filter is documented in genesis/lib/structure/search.php */
-			'label'        => apply_filters( 'genesis_search_form_label', __( 'Search site', 'utility-pro' ) ),
+			'label'        => apply_filters( 'genesis_search_form_label', __( 'Search site', 'utility-pro' ) ), // WPCS: prefix ok.
 			// Placeholder should be empty: http://www.nngroup.com/articles/form-design-placeholders/.
 			/** This filter is documented in genesis/lib/structure/search.php */
-			'placeholder'  => apply_filters( 'genesis_search_text', __( '', 'utility-pro' ) ),
+			'placeholder'  => apply_filters( 'genesis_search_text', __( '', 'utility-pro' ) ), // WPCS: prefix ok.
 			/** This filter is documented in genesis/lib/structure/search.php */
-			'button'       => apply_filters( 'genesis_search_button_text', __( 'Search', 'utility-pro' ) ),
+			'button'       => apply_filters( 'genesis_search_button_text', __( 'Search', 'utility-pro' ) ), // WPCS: prefix ok.
 			/**
 			 * Filter the ARIA label for the search form button.
 			 *
@@ -53,8 +57,8 @@ class Utility_Pro_Search_Form {
 			 *
 			 * @param string $button_label Default label is "Search".
 			 */
-			'button_label' => apply_filters( 'genesis_search_button_label', __( 'Search', 'utility-pro' ) ),
-		);
+			'button_label' => apply_filters( 'genesis_search_button_label', __( 'Search', 'utility-pro' ) ), // WPCS: prefix ok.
+		];
 
 		$this->strings = wp_parse_args( $strings, $default_strings );
 	}
@@ -68,7 +72,7 @@ class Utility_Pro_Search_Form {
 	 *
 	 * @return string|array String if key is not 'all', and key exists. Array of all strings otherwise.
 	 */
-	protected function strings( $key = 'all' ) {
+	protected function strings( string $key = 'all' ) {
 		if ( 'all' !== $key && isset( $this->strings[ $key ] ) ) {
 			return $this->strings[ $key ];
 		}
@@ -83,7 +87,7 @@ class Utility_Pro_Search_Form {
 	 *
 	 * @return string Form markup.
 	 */
-	public function get_form() {
+	public function get_form() : string {
 		return sprintf(
 			'<form method="get" action="%s" role="search" class="search-form">%s</form>',
 			esc_url( home_url( '/' ) ),
@@ -98,7 +102,7 @@ class Utility_Pro_Search_Form {
 	 *
 	 * @return string Label markup.
 	 */
-	protected function get_label() {
+	protected function get_label() : string {
 		return sprintf(
 			'<label for="%s" class="screen-reader-text">%s</label>',
 			esc_attr( $this->get_id() ),
@@ -113,8 +117,8 @@ class Utility_Pro_Search_Form {
 	 *
 	 * @return string Input field markup.
 	 */
-	protected function get_input() {
-		$value = get_search_query() ? apply_filters( 'the_search_query', get_search_query() ) : '';
+	protected function get_input() : string {
+		$value = get_search_query() ? apply_filters( 'the_search_query', get_search_query() ) : ''; // WPCS: prefix ok.
 
 		return sprintf(
 			'<input type="search" name="s" id="%s" value="%s" placeholder="%s" autocomplete="off" />',
@@ -131,7 +135,7 @@ class Utility_Pro_Search_Form {
 	 *
 	 * @return string Button markup.
 	 */
-	protected function get_button() {
+	protected function get_button() : string {
 		return sprintf(
 			'<button type="submit" aria-label="%s"><span class="search-button-text">%s</span></button>',
 			esc_attr( $this->strings( 'button_label' ) ),
@@ -146,8 +150,8 @@ class Utility_Pro_Search_Form {
 	 *
 	 * @return string Unique ID.
 	 */
-	protected function get_id() {
-		if ( ! isset( $this->unique_id ) ) {
+	protected function get_id() : string {
+		if ( null === $this->unique_id ) {
 			$this->unique_id = 'searchform-' . uniqid( '', true );
 		}
 
