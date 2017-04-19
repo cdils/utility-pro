920c3734ce9821413bb319e98388bbb93a9a31ca
Gary Jones <gary@gamajo.com>
2017-04-19 14:20:32 +0100

    Move enqueueing of assets into functions.php
    This has been a tricky file to handle in the way I want, so for now, it’s being merged into `functions.php`.

--- functions.php
+++ functions.php
@@ -19,14 +19,14 @@ use BrightNucleus\Config\ConfigFactory;
 use CDils\UtilityPro\GoogleFont\Enriqueta;
 use CDils\UtilityPro\GoogleFont\OpenSans;
 
-load_child_theme_textdomain( 'utility-pro', get_stylesheet_directory() . '/languages' );
+\load_child_theme_textdomain( 'utility-pro', \get_stylesheet_directory() . '/languages' );
 
 // Autoload dependencies.
-if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+if ( \file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
 }
 
-define( 'UTILITY_PRO_CONFIG_DIR', __DIR__ . '/config/' );
+\define( 'UTILITY_PRO_CONFIG_DIR', __DIR__ . '/config/' );
 
 add_action( 'genesis_setup', __NAMESPACE__ . '\\setup', 15 );
 /**
@@ -38,11 +38,11 @@ add_action( 'genesis_setup', __NAMESPACE__ . '\\setup', 15 );
  * @since 1.0.0
  */
 function setup() {
-	define( 'CHILD_THEME_NAME', 'utility-pro' ); // WPCS: prefix ok.
-	define( 'CHILD_THEME_URL', 'https://store.carriedils.com/utility-pro' );  // WPCS: prefix ok.
-	define( 'CHILD_THEME_VERSION', '1.3.1' );  // WPCS: prefix ok.
+	\define( 'CHILD_THEME_NAME', 'utility-pro' ); // WPCS: prefix ok.
+	\define( 'CHILD_THEME_URL', 'https://store.carriedils.com/utility-pro' );  // WPCS: prefix ok.
+	\define( 'CHILD_THEME_VERSION', '1.3.1' );  // WPCS: prefix ok.
 
-	$config_file = UTILITY_PRO_CONFIG_DIR . 'defaults.php';
+	$config_file = \UTILITY_PRO_CONFIG_DIR . 'defaults.php';
 	$config = ConfigFactory::createSubConfig( $config_file, 'CDils\UtilityPro' );
 
 	// Register theme support items, defined in config/defaults.php.
@@ -50,15 +50,15 @@ function setup() {
 	$theme_support->register();
 
 	// Add custom image sizes.
-	add_image_size( 'feature-large', 960, 330, true );
+	\add_image_size( 'feature-large', 960, 330, true );
 
 	// Unregister secondary sidebar.
-	unregister_sidebar( 'sidebar-alt' );
+	\unregister_sidebar( 'sidebar-alt' );
 
 	// Unregister layouts that use secondary sidebar.
-	genesis_unregister_layout( 'content-sidebar-sidebar' );
-	genesis_unregister_layout( 'sidebar-content-sidebar' );
-	genesis_unregister_layout( 'sidebar-sidebar-content' );
+	\genesis_unregister_layout( 'content-sidebar-sidebar' );
+	\genesis_unregister_layout( 'sidebar-content-sidebar' );
+	\genesis_unregister_layout( 'sidebar-sidebar-content' );
 
 	add_filter( 'theme_page_templates',  __NAMESPACE__ . '\\remove_genesis_page_templates' );
 
@@ -106,6 +106,8 @@ function setup() {
 		add_filter( 'genesis_author_box_gravatar_size', function() {
 			return 96;
 		} );
+
+		add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_assets' );
 	}// End if().
 }
 
@@ -121,5 +123,75 @@ function remove_genesis_page_templates( array $page_templates ) : array {
 	return $page_templates;
 }
 
-// Add scripts to enqueue.
-include get_stylesheet_directory() . '/includes/enqueue-assets.php';
+/**
+ * Enqueue theme assets.
+ *
+ * @since 1.0.0
+ */
+function enqueue_assets() {
+	$suffix = \defined( 'WP_DEBUG' ) && WP_DEBUG ? '.min.js' : '.js';
+
+	// Replace style.css with style-rtl.css for RTL languages.
+	\wp_style_add_data( 'utility-pro', 'rtl', 'replace' );
+
+	// Keyboard navigation (dropdown menus) script.
+	\wp_enqueue_script(
+		'utility-pro-keyboard-dropdown',
+		\get_stylesheet_directory_uri() . '/js/keyboard-dropdown' . $suffix,
+		[ 'jquery' ],
+		\CHILD_THEME_VERSION,
+		true
+	);
+
+	// Load mobile responsive menu.
+	\wp_enqueue_script(
+		'utility-pro-responsive-menu',
+		\get_stylesheet_directory_uri() . '/js/responsive-menu' . $suffix,
+		[ 'jquery' ],
+		'1.0.0', true
+	);
+
+	$localize_primary = [
+		'buttonText'     => __( 'Menu', 'utility-pro' ),
+		'buttonLabel'    => __( 'Primary Navigation Menu', 'utility-pro' ),
+		'subButtonText'  => __( 'Sub Menu', 'utility-pro' ),
+		'subButtonLabel' => __( 'Sub Menu', 'utility-pro' ),
+	];
+
+	$localize_footer = [
+		'buttonText'     => __( 'Footer Menu', 'utility-pro' ),
+		'buttonLabel'    => __( 'Footer Navigation Menu', 'utility-pro' ),
+		'subButtonText'  => __( 'Sub Menu', 'utility-pro' ),
+		'subButtonLabel' => __( 'Sub Menu', 'utility-pro' ),
+	];
+
+	// Localize the responsive menu script (for translation).
+	\wp_localize_script( 'utility-pro-responsive-menu', 'utilityProMenuPrimaryL10n', $localize_primary );
+	\wp_localize_script( 'utility-pro-responsive-menu', 'utilityProMenuFooterL10n', $localize_footer );
+
+	\wp_enqueue_script(
+		'utility-pro-responsive-menu-args',
+		\get_stylesheet_directory_uri() . '/js/responsive-menu-args' . $suffix,
+		[ 'utility-pro-responsive-menu' ],
+		\CHILD_THEME_VERSION,
+		true
+	);
+
+	// Load Backstretch scripts only if custom background is being used
+	// and we're on the home page or a page using the landing page template.
+	if ( ! \get_background_image() || ( ! ( \is_front_page() || \is_page_template( 'page_landing.php' ) ) ) ) {
+		return;
+	}
+
+	\wp_enqueue_script(
+		'utility-pro-backstretch',
+		\get_stylesheet_directory_uri() . '/js/backstretch' . $suffix,
+		[ 'jquery' ],
+		'2.0.1',
+		true
+	);
+
+	\wp_localize_script( 'utility-pro', 'utilityProBackstretchL10n', [
+		'src' => \get_background_image(),
+	] );
+}
